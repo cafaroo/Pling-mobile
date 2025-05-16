c904a8f6efe43a572b14eaaa0958969a9d39bd73
cafaroo <92196230+cafaroo@users.noreply.github.com>
2025-05-16 12:57:57 +0200

    Lagt till bakåtkompatibilitetswrapper för UserTeamsScreen

---
+++ src/ui/user/screens/UserTeamsScreen.tsx
@@ -0,0 +1,5 @@
+// Denna fil är bevarad för bakåtkompatibilitet
+// Kommer att fasas ut när alla importer har migrerats till den nya strukturen
+
+import { UserTeamsScreenContainer } from './UserTeamsScreen';
+export { UserTeamsScreenContainer as UserTeamsScreen }; 
